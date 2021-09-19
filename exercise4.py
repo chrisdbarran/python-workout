ab16d00faab897d9985e188e9d1bb6d5d8c71af8
Chris Barran <chris.d.barran@gmail.com>
2021-09-19 10:38:38 +0100

    Additional exercises

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def hex_output(hex_number: str) -> int:
     hexs = "0123456789ABCDEF"
     decimal_value = 0
@@ -5,6 +8,20 @@ def hex_output(hex_number: str) -> int:
         val = hexs.index(num)
         decimal_value += val * (16 ** power)
     return decimal_value
+
+
+def hex_ord_chr(hex_number: str) -> int:
+    arg = f"0x{hex_number}"
+    return ord(chr(arg))
+
+
+def name_triangle(name: str) -> List[str]:
+    triangle = []
+    for i in range(0, len(name)):
+        sub = name[:i + 1]
+        triangle.append(sub)
+        print(sub)
+    return triangle
 
 
 if __name__ == "__main__":
--- tests/test_exercise4.py
+++ tests/test_exercise4.py
@@ -4,3 +4,15 @@ import exercise4 as ex
 def test_hex_number():
     assert ex.hex_output("50") == 80
     assert ex.hex_output("FF") == 255
+
+    
+def test_hex_ord_chr():
+    assert ex.hex_output("50") == 80
+    assert ex.hex_output("FF") == 255
+
+
+def test_name_triangle():
+    # act
+    triangle = ex.name_triangle("Don Quixote")
+    assert triangle[0] == "D"
+    assert triangle[10] == "Don Quixote"
