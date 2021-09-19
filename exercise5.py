96aa41bcb8485d6cd3b0bcd517ac5d27e201336d
Chris Barran <chris.d.barran@gmail.com>
2021-09-19 11:18:03 +0100

    Exercise 5

---
+++ exercise5.py
@@ -0,0 +1,17 @@
+def pig_latin(word: str) -> str:
+    if word[0] in 'aeiou':
+        return f"{word}way"
+    return f"{word[1:]}{word[0]}ay"
+
+
+def pig_latin_ternary(word: str) -> str:
+    return f"{word}way" if word[0] in 'aeiou' else f"{word[1:]}{word[0]}ay"
+
+
+def pig_latin_capitalised(word: str) -> str:
+    return pig_latin(word).capitalize() if word[0].isupper() else pig_latin(word)
+
+
+if __name__ == "__main__":
+    word = input("Enter a word: ")
+    print(f"Pig Latin: {pig_latin(word)}")
---
+++ tests/test_exercise5.py
@@ -0,0 +1,19 @@
+import exercise5 as ex
+
+
+def test_pig_latin_non_vowel():
+    assert ex.pig_latin("computer") == "omputercay"
+
+
+def test_pig_latin_vowel():
+    assert ex.pig_latin("air") == "airway"
+
+
+def test_pig_latin_ternary():
+    assert ex.pig_latin_ternary("computer") == "omputercay"
+    assert ex.pig_latin_ternary("air") == "airway"
+
+
+def test_pig_latin_capitalised():
+    assert ex.pig_latin_capitalised("Computer") == "Omputercay"
+    assert ex.pig_latin_capitalised("computer") == "omputercay"
