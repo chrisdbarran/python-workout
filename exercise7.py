98252835b0fc00b3010ec36567f36dc58d8caff8
Chris Barran <chris.d.barran@gmail.com>
2021-10-01 19:37:03 +0100

    Exercise 7 extra

--- exercise7.py
+++ exercise7.py
@@ -2,11 +2,15 @@
 def ubbi_dubbi(word: str):
     # every vowel prefaced with ub
     output = []
-    vowels = 'aeiou'
+    vowels = 'aeiouAEIOU'
 
     for letter in word:
         if letter in vowels:
             output.append('ub')
         output.append(letter)
 
-    return ''.join(output) 
+    return ''.join(output)
+
+def ubbi_dubbi_capitalize(word: str):
+
+    return ubbi_dubbi(word).capitalize() if word[0].isupper() else ubbi_dubbi(word)
--- tests/test_exercise7.py
+++ tests/test_exercise7.py
@@ -6,3 +6,11 @@ def test_ubbi_dubbi():
     assert ex.ubbi_dubbi("octopus") == "uboctubopubus"
     assert ex.ubbi_dubbi("elephant") == "ubelubephubant"
     assert ex.ubbi_dubbi("soap") == "suboubap"
+
+
+def test_ubbi_dubbi_capitalize():
+    assert ex.ubbi_dubbi_capitalize("Milk") == "Mubilk"
+    assert ex.ubbi_dubbi_capitalize("program") == "prubogrubam"
+    assert ex.ubbi_dubbi_capitalize("Octopus") == "Uboctubopubus"
+    assert ex.ubbi_dubbi_capitalize("elephant") == "ubelubephubant"
+    assert ex.ubbi_dubbi_capitalize("soap") == "suboubap"
