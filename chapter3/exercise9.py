0af32367882142e5e0a3342bc8a994acc69f164d
Chris Barran <chris.d.barran@gmail.com>
2021-10-02 10:03:52 +0100

    Exercise 9 add part 2

--- chapter3/exercise9.py
+++ chapter3/exercise9.py
@@ -3,3 +3,17 @@ from typing import Iterable
 
 def firstlast(iterable: Iterable):
     return iterable[:1] + iterable[-1:]
+
+def even_odd_sums(numbers):
+    evens = [num for (i, num) in enumerate(numbers) if i % 2 == 0 ]
+    odds = [num for (i, num) in enumerate(numbers) if i % 2 > 0 ]
+    return [sum(evens), sum(odds)]
+
+def plus_minus(numbers):
+    total = numbers[:1][0]
+    for (i, num) in enumerate(numbers[1:]):
+        if(i%2):
+            total -= num
+        else:
+            total += num
+    return total
--- tests/test_exercise9.py
+++ tests/test_exercise9.py
@@ -1,6 +1,25 @@
+import pytest
 import chapter3.exercise9 as ex
+
+@pytest.fixture
+def numbers_list():
+    return [10,20,30,40,50,60]
+
+@pytest.fixture
+def numbers_tuple():
+    return (10,20,30,40,50,60)
 
 def test_first_last():
     assert ex.firstlast('abc') == 'ac'
     assert ex.firstlast([1,2,3,4]) == [1,4]
     assert ex.firstlast(('a','b','c','d')) == ('a', 'd')
+
+
+def test_even_odd_sums(numbers_list, numbers_tuple):
+    assert ex.even_odd_sums(numbers_list) == [90,120]
+    assert ex.even_odd_sums(numbers_tuple) == [90,120]
+
+
+def test_plus_minus(numbers_list, numbers_tuple):
+    assert ex.plus_minus(numbers_list) == 50
+    assert ex.plus_minus(numbers_tuple) == 50
