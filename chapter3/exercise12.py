ca5a37f8d53561a94776bf68f918cd6f84415fa9
Chris Barran <chris.d.barran@gmail.com>
2021-10-17 16:14:51 +0100

    Exercise12 additional

--- chapter3/exercise12.py
+++ chapter3/exercise12.py
@@ -6,3 +6,11 @@ def most_repeating_word(words: Sequence[str]) -> str:
 
 def most_repeating_letter_count(word: str) -> int:
     return max(Counter(word).values())
+
+def most_repeating_vowel(words: Sequence[str]) -> str:
+    return max(words, key=most_repeating_vowel_count)
+
+def most_repeating_vowel_count(word: str) -> int:
+    counts = Counter(word)
+    vowels = {key: counts[key] for key in counts.keys() & {'a','e','i','o','u'}}
+    return max(vowels.values())
--- tests/test_exercise12.py
+++ tests/test_exercise12.py
@@ -3,3 +3,7 @@ import chapter3.exercise12 as ex
 def test_most_repeating_word():
     words = ['this', 'is', 'an', 'elementary', 'test', 'example']
     assert ex.most_repeating_word(words) == 'elementary'
+
+def test_most_repeating_vowel():
+    words = ['this', 'is', 'an', 'elementary', 'test', 'example']
+    assert ex.most_repeating_vowel(words) == 'elementary'
